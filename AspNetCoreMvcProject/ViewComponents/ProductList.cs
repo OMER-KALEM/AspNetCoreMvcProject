86782cdbd0e9295f00ac7a810cfcee4af7a3ad6d
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-12-21 14:14:02 +0300

    Commit commit = new Commit(2,"ProductList View Component")

---
+++ AspNetCoreMvcProject/ViewComponents/ProductList.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCoreMvcProject.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMvcProject.ViewComponents
+{
+    public class ProductList : ViewComponent
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductList(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_productRepository.GetAll());
+        }
+    }
+}
