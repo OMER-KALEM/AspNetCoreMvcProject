cbb66afcbcf9173e0cc9bc37da9875c9a64d3b86
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-12-24 01:23:39 +0300

    Commit commit = new Commit(5,"TagHelpers")

---
+++ AspNetCoreMvcProject/TagHelpers/CategoryName.cs
@@ -0,0 +1,32 @@
+﻿using AspNetCoreMvcProject.Interfaces;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcProject.TagHelpers
+{
+    [HtmlTargetElement("getCategoryName")]
+    public class CategoryName : TagHelper
+    {
+        public int ProductId { get; set; }
+        private readonly IProductRepository _productRepository;
+        public CategoryName(IProductRepository productRepository)
+        {
+             _productRepository = productRepository;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var data = "";
+            var categories = _productRepository.GetCategories(ProductId).Select(I=>I.CategoryName);
+            foreach (var category in categories)
+            {
+                data += category + "";
+            }
+
+            output.Content.SetContent(data);
+        }
+    }
+}
