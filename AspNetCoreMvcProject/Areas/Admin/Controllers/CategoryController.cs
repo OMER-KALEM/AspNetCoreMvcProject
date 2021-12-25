0ed2560c3ae3bd67c7baeae1039610531803c212
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-12-25 19:36:32 +0300

    Commmit commit = new Commit(18,"Category List");

---
+++ AspNetCoreMvcProject/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreMvcProject.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvcProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_categoryRepository.GetAll());
+        }
+    }
+}
