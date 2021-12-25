2d46c724723fe871f83de2f471b0a78e9d4cd24f
Ömer KALEM <omer.kalem@ogr.sakarya.edu.tr>
2021-12-25 17:49:46 +0300

    Commmit commit = new Commit(15,"AddProduct Post")

--- AspNetCoreMvcProject/Areas/Admin/Controllers/HomeController.cs
+++ AspNetCoreMvcProject/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using AspNetCoreMvcProject.Interfaces;
+﻿using AspNetCoreMvcProject.Entities;
+using AspNetCoreMvcProject.Interfaces;
+using AspNetCoreMvcProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,6 +25,44 @@ namespace AspNetCoreMvcProject.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_productRepository.GetAll());
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View(new AddProductModel());
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(AddProductModel addProductModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = new Product();
+                if (addProductModel.Image != null) //&& addProductModel.Image.ContentType == "image/jpeg"
+                {
+                    try
+                    {
+                        var contentType = Path.GetExtension(addProductModel.Image.FileName);
+                        var imageName = Guid.NewGuid() + contentType;
+                        var toUpload = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/" + imageName);
+                        var stream = new FileStream(toUpload, FileMode.Create);
+                        addProductModel.Image.CopyTo(stream);
+                        product.Image = imageName;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+
+                product.ProductName = addProductModel.ProductName;
+                product.UnitPrice = addProductModel.UnitPrice;
+                _productRepository.Add(product);
+
+                //return RedirectToAction("Index","Home",new { area="Admin"});
+                return RedirectToAction("Index");
+            }
+            return View(addProductModel);
         }
     }
 }
--- AspNetCoreMvcProject/Entities/Product.cs
+++ AspNetCoreMvcProject/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper.Contrib.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace AspNetCoreMvcProject.Entities
 {
+    [Table("Products")]
     public class Product
     {
         public int ProductId { get; set; }
